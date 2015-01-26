aa4c9c26d7a11e89860147a8b12c74a2c4b78c7d
esencial <sergio.gonzalez@esencialsistemas.com>
2015-01-26 19:25:19 +0100

    Prueba de gitignore

--- config/settings.php
+++ config/settings.php
@@ -10,24 +10,24 @@ $settings__charset="UTF-8";
 
 // Web server document root, e.g. /srv/www/htdocs
 // no trailing slash!
-$settings__root_to_server="/srv/www/htdocs";
+$settings__root_to_server="/Usuarios/silvia/Documentos/ORSEE/forksee/forksee_local";
 
 
 // Experiment system root relative to server root, e.g. /orsee
 // begins always with "/" if in a subdirectory
 // no trailing slash!
-$settings__root_directory="/orsee-2.2.0";
+$settings__root_directory="/";
 
 // url to web server document root (IP or domain name)
 // without trailing slash and the http://!
-$settings__server_url="127.0.0.1";
+$settings__server_url="forksee.local";
 
 // Database configuration. Don't forget to create the database
 $site__database_host="localhost";
 //$site__database_port="3306"; // set only if not default 3306
-$site__database_database="orsee_2_2_0_db";
-$site__database_admin_username="orsee_2_2_0_user";
-$site__database_admin_password="orsee_2_2_0_pw";
+$site__database_database="forksee_db";
+$site__database_admin_username="root";
+$site__database_admin_password="root";
 $site__database_type="mysql";
 $site__database_table_prefix="or_";
 

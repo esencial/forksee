18d9e91e231272b37004399923fccab5bc109d43
esencial <sergio.gonzalez@esencialsistemas.com>
2015-02-04 14:15:27 +0100

    Archivos para insertar un CAPTCHA

--- public/contact.php
+++ public/contact.php
@@ -10,12 +10,31 @@ echo '<BR><BR>
 	echo '</h4>
 		<BR>
 		<TABLE width=80%><TR><TD>';
-	echo content__get_content("contact");
+	//echo content__get_content("contact");
+        //llamamos a una función que nos dibuje el formulario
+        formulario_contacto ();
 	echo '
 		</TD></TR></TABLE>
 
 		</center>';
 
 include ("footer.php");
+
+function formulario_contacto() {
+    global $lang;
+    $this_lang=$lang['lang'];
+    echo '<form action="#" method="get">';
+    echo $lang['name'];
+    echo '<input type="text" name="fname"><br>';
+    echo $lang['email'];
+    echo '<input type="text" name="email"><br>';
+    echo $lang['email_subject'];
+    echo '<input type="text" name="subject"><br>';
+    echo $lang['body_of_message'];
+    echo '<textarea rows="4" cols="50" type="text" name="message"></textarea><br>';
+    echo '<input type="submit" value="Submit">';
+    echo '</form>';   
+    
+}
 
 ?>

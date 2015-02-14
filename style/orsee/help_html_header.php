751640202ecf11f3acc414811b6502299fcf7f45
Silvia <silvia.suria@esencialsistemas.com>
2015-02-14 20:21:42 +0100

    limpiar código html de estilos en línea

--- style/orsee/help_html_header.php
+++ style/orsee/help_html_header.php
@@ -1,28 +1,28 @@
 <center>
-<TABLE BORDER=0 CELLSPACING=0 CELLPADDING=0 WIDTH="100%">
-	<TR VALIGN=TOP ALIGN=LEFT BGCOLOR="#8080FF">
-	<TD COLSPAN=2 WIDTH=100% HEIGHT=10 ALIGN=RIGHT VALIGN=BOTTOM>
+<TABLE>
+	<TR>
+	<TD>
 	&nbsp;
 	</TD>
 	</TR>
 
-	<TR bgcolor="#222222">
-		<TD valign=middle align=left>
+	<TR>
+		<TD>
 		<IMG border=0 SRC="<?php echo $settings__root_url; ?>/style/orsee/help_logo.gif">
 		</TD>
-	<TD valign=middle align=center>
-		<FONT face="Arial" COLOR="#FFFFFF"></FONT>
+	<TD>
+		
 	</TD>
 	</TR>
 
-        <TR VALIGN=TOP ALIGN=LEFT BGCOLOR="#8080FF">
-        <TD COLSPAN=2 WIDTH=100% HEIGHT=10 ALIGN=RIGHT VALIGN=BOTTOM>
+        <TR>
+        <TD>
         &nbsp;
         </TD>   
         </TR>
-	<TR><TD bgcolor=snow colspan=2>&nbsp;</TD></TR>
-	<TR><TD width=100% bgcolor=snow colspan=2 width=100%>
-	<TABLE width=100%>
+	<TR><TD>&nbsp;</TD></TR>
+	<TR><TD>
+	<TABLE>
 	<TR><TD>&nbsp;&nbsp;</TD>
 	<TD>
 

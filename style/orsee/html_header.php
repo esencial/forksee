ab42a5c60fa7d5f64dcc4bb8af75351418ec88c4
Silvia <silvia.suria@esencialsistemas.com>
2015-02-14 20:24:49 +0100

    limpiar código html de estilos en línea

--- style/orsee/html_header.php
+++ style/orsee/html_header.php
@@ -1,30 +1,29 @@
 <center>
 <TABLE>
-	<TR VALIGN=TOP ALIGN=LEFT BGCOLOR="#011E62">
-		<TD COLSPAN=2 WIDTH=100% HEIGHT=19 ALIGN=RIGHT VALIGN=BOTTOM>
-		<P><FONT face="Arial" size=1 class="small" COLOR="#FFFFFF">FORKSEE</FONT>
+	<TR>
+		<TD>
+		FORKSEE
 		</TD>
 	</TR>
 
 
 	<TR class="cabecera">
-		<TD WIDTH=100% HEIGHT=70 VALIGN=MIDDLE ALIGN=LEFT rowspan=2>
-			<TABLE BORDER=0 CELLSPACING=0 CELLPADDING=0 WIDTH=300>
+		<TD>
+			<TABLE>
 			<TR>
-				<TD VALIGN=TOP>
+				<TD>
 				<IMG border=0 SRC="../style/orsee/orsee_sign.png">
 				</TD>
 			</TR>
 			</TABLE>
 		</TD>
-                <TD ALIGN=RIGHT VALIGN=bottom WIDTH="100%">
-                        <TABLE BORDER=0 CELLSPACING=0 CELLPADDING=0 WIDTH="100%" HEIGHT="100%">
+                <TD>
+                        <TABLE>
 
 
                         <TR>
-                                <TD valign=bottom align=right>
-                                <!--<IMG
-                                SRC="../style/orsee/orsee_logo.gif" BORDER=0>-->&nbsp;
+                                <TD>
+                                
                                 </TD>
                         </TR>
                         </TABLE>
@@ -33,22 +32,22 @@
 	</TR>
 
 	<TR class="cabecera">
-                <TD valign=top align=right>
-                        <FONT face="Arial" size=1 class="small" COLOR="#FFFFFF">THE EASE OF RECRUITMENT</FONT>
+                <TD>
+                        THE EASE OF RECRUITMENT
                 </TD>
 
 	</TR>
 
 	<TR>
-		<TD VALIGN=TOP ALIGN=LEFT colspan=2>
+		<TD>
 
-			<TABLE BORDER=0 CELLSPACING=0 CELLPADDING=0 WIDTH="100%" HEIGHT="100%">
+			<TABLE>
 			<TR>
-				<TD width=10 bgcolor="#011E62">
+				<TD>
 					<IMG src="../style/orsee/clearpixel.gif" border=0 height=500 width=10>
 				</TD>
 
-				<TD width=140 bgcolor="#011E62" valign=top align=center>
+				<TD>
 					<IMG src="../style/orsee/clearpixel.gif" border=0 height=1 width=140>
 					<BR>
 					<?php navigation(); ?>

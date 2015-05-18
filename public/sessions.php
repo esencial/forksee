48ea0a45dcfc8531d8e7b17a96a8e8175cc4fc80
Silvia <silvia.suria@esencialsistemas.com>
2015-05-18 23:56:46 +0200

    añadir login para el participante

--- public/sessions.php
+++ public/sessions.php
@@ -4,14 +4,19 @@ ob_start();
 session_start();// Starting Session
 // Storing Session
 $user_check=$_SESSION['login_user'];
-// SQL Query To Fetch Complete Information Of User
-$query="SELECT email FROM ".table('participants')." WHERE email='$username'";
-$query = orsee_query($query,"");
+if(!isset($user_check)){
+    exit();
+} else {
+    echo "user name ".$user_check;
+    // SQL Query To Fetch Complete Information Of User
+    $query="SELECT email FROM ".table('participants')." WHERE email='$username'";
+    $query = orsee_query($query,"");
 
-$row = mysql_fetch_assoc($query);
-$login_session =$row['email'];
-if(!isset($login_session)){
-mysql_close($connection); // Closing Connection
-header('Location: index.php'); // Redirecting To Home Page
+    $row = mysql_fetch_assoc($query);
+    $login_session =$row['email'];
+    if(!isset($login_session)){
+        mysql_close($connection); // Closing Connection
+        header('Location: index.php'); // Redirecting To Home Page
+    }
 }
 ?>

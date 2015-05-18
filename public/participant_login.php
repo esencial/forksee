a7084856d157c98d473f24dc93b94f92ab85eafe
Silvia <silvia.suria@esencialsistemas.com>
2015-05-18 22:34:51 +0200

    añadir login para el participante

--- public/participant_login.php
+++ public/participant_login.php
@@ -1,10 +1,29 @@
 <?php
+include('login.php'); // Includes Login Script
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+if(isset($_SESSION['login_user'])){
+header("location: profile.php");
+}
 ?>
-
+<!DOCTYPE html>
+<html>
+<head>
+<title>Login form for participants</title>
+</head>
+<body>
+<div id="main">
+<h1>Login</h1>
+<div id="login">
+<h2>Login Form</h2>
+<form action="" method="post">
+<label>UserName :</label>
+<input id="name" name="username" placeholder="username" type="text">
+<label>Password :</label>
+<input id="password" name="password" placeholder="**********" type="password">
+<input name="submit" type="submit" value=" Login ">
+<span><?php echo $error; ?></span>
+</form>
+</div>
+</div>
+</body>
+</html>

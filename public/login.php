2c1495be44633e3c5857c906b750a49662bc4d98
Silvia <silvia.suria@esencialsistemas.com>
2015-05-28 23:20:37 +0200

    corregir SQL logado

--- public/login.php
+++ public/login.php
@@ -12,20 +12,16 @@ else
 	// Define $username and $password
 	$username=$_POST['username'];
 	$password=$_POST['password'];
-	// Establishing Connection with Server by passing server_name, user_id and password as a parameter
+	// Establishing Connection 
 	
 	// To protect MySQL injection for Security purpose
 	$username = stripslashes($username);
 	$password = stripslashes($password);
 	$username = mysql_real_escape_string($username);
 	$password = md5(mysql_real_escape_string($password));
-	// Selecting Database
-	//$db = mysql_select_db("company", $connection);
-	// SQL query to fetch information of registerd users and finds user match.
-	//$query = mysql_query("select * from login where password='$password' AND username='$username'", $connection);
 
 	$query1 = "SELECT * FROM ".table('participants')." WHERE password='$password' AND identification_number='$username'";
-	$result=orsee_query($query);
+	$result=orsee_query($query1);
 	
 	
 	if (!is_null($result)) {

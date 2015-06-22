e1ddb1b23b932f729cfae5654d11bc40a10147df
Silvia <silvia.suria@esencialsistemas.com>
2015-06-23 00:12:07 +0200

    corregir listado. Salían 2 cabeceras

--- admin/subject_main.php
+++ admin/subject_main.php
@@ -22,17 +22,27 @@ include ("header.php");
         <?php
          $query="SELECT * FROM or_subjects";  
          $result=mysqli_query($GLOBALS['mysqli'],$query) or die("Database error: " . mysqli_error($GLOBALS['mysqli']));
+		echo "<table width=200><tr><th>Nº</th><th>Asignatura</th><th>Grado</th><th>Curso</th><th>Créditos</th></tr>";
          while($query2=mysqli_fetch_assoc($result))
             {
                 $id=$query2['subject_id'];
                 $desc=$query2['subject_desc'];
-                echo "<table width=200><tr><td width=20>".$id."</td>";
+                $degree=$query2['degree'];
+                $year=$query2['year'];
+                $credits=$query2['credits'];
+                
+                
+                echo "<tr><td width=20>".$id."</td>";
                 echo "<td width=20>".$desc."</td>";
+                echo "<td width=20>".$degree."</td>";
+                echo "<td width=20>".$year."</td>";
+                echo "<td width=20>".$credits."</td>";
                 echo "<td width=20><a href='subject_edit.php?id=".$id.
                         "&desc=".$desc."'>Edit</a></td>";                
-                echo "<td width=20><a href='javascript:confirmDelete(\"subject_delete.php?id=".$id."\", \"".$desc."\")'>Delete</a></td><tr></table>"; 
+                echo "<td width=20><a href='javascript:confirmDelete(\"subject_delete.php?id=".$id."\", \"".$desc."\")'>Delete</a></td><tr>"; 
             }
-         echo "<br><a href='subject_add.php'>Add subject</a>";   
+
+         echo "</table><br><a href='subject_add.php'>Add subject</a>";   
         ?>
         
         
